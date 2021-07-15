88f1c06b738eca45892b83d4080e52f267c38471
bastihz <bastihz.dev@posteo.de>
2021-07-15 17:47:04 +0200

    Set sessioncreationpolicy to stateless

--- Recipes/src/main/java/recipes/SecurityConfig.java
+++ Recipes/src/main/java/recipes/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -27,6 +28,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
             .csrf().disable()
             .headers().frameOptions().disable();
+
+        // Ensure that only a single instance of a user is authenticated at a time. And if the
+        // same user tries to access the url then it's previous session is terminated and then
+        // the user has to provide login credentials again for which new session is created.
+        // https://stackoverflow.com/a/64154354
+        http
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Bean
