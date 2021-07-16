ad8acafc17df9a85496578ec6bc2f81435f0ea13
bastihz <bastihz.dev@posteo.de>
2021-07-16 16:17:34 +0200

    Slightly improve the code fix the grammar and some bugs and improve the style a bit

--- Coffee_Machine/src/main/java/coffeemachine/CoffeeMachine.java
+++ Coffee_Machine/src/main/java/coffeemachine/CoffeeMachine.java
@@ -3,69 +3,69 @@ package coffeemachine;
 import java.util.Scanner;
 
 public class CoffeeMachine {
-    final static int waterPerCupOfEspresso = 250;
-    final static int milkPerCupOfEspresso = 0;
-    final static int beansPerCupOfEspresso = 16;
-    final static int costPerCupOfEspresso = 4;
 
-    final static int waterPerCupOfLatte = 350;
-    final static int milkPerCupOfLatte = 75;
-    final static int beansPerCupOfLatte = 20;
-    final static int costPerCupOfLatte = 7;
+    private static final int WATER_PER_CUP_OF_ESPRESSO = 250;
+    private static final int MILK_PER_CUP_OF_ESPRESSO = 0;
+    private static final int BEANS_PER_CUP_OF_ESPRESSO = 16;
+    private static final int COST_PER_CUP_OF_ESPRESSO = 4;
 
-    final static int waterPerCupOfCappuccino = 200;
-    final static int milkPerCupOfCappuccino = 100;
-    final static int beansPerCupOfCappuccino = 12;
-    final static int costPerCupOfCappuccino = 6;
+    private static final int WATER_PER_CUP_OF_LATTE = 350;
+    private static final int MILK_PER_CUP_OF_LATTE = 75;
+    private static final int BEANS_PER_CUP_OF_LATTE = 20;
+    private static final int COST_PER_CUP_OF_LATTE = 7;
 
-    int storedWater = 400;
-    int storedMilk = 540;
-    int storedBeans = 120;
-    int storedCups = 9;
-    int storedMoney = 550;
+    private static final int WATER_PER_CUP_OF_CAPPUCCINO = 200;
+    private static final int MILK_PER_CUP_OF_CAPPUCCINO = 100;
+    private static final int BEANS_PER_CUP_OF_CAPPUCCINO = 12;
+    private static final int COST_PER_CUP_OF_CAPPUCCINO = 6;
 
-    int requiredWater;
-    int requiredMilk;
-    int requiredBeans;
-    int requiredMoney;
+    private int storedWater = 400;
+    private int storedMilk = 540;
+    private int storedBeans = 120;
+    private int storedCups = 9;
+    private int storedMoney = 120;
 
-    enum State {
+    private int requiredWater;
+    private int requiredMilk;
+    private int requiredBeans;
+    private int requiredMoney;
+
+    private enum State {
         CHOOSING_ACTION, CHOOSING_PRODUCT,
         FILLING_WATER, FILLING_MILK, FILLING_BEANS, FILLING_CUPS,
         FINISHED
     }
-    State state = State.CHOOSING_ACTION;
+    private State state = State.CHOOSING_ACTION;
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        CoffeeMachine coffeeMachine = new CoffeeMachine();
-        coffeeMachine.mainLoop(scanner);
+    private final Scanner scanner = new Scanner(System.in);
+
+    public static void main(final String[] args) {
+        new CoffeeMachine().run();
     }
 
-    void mainLoop(Scanner scanner) {
+    void run() {
         while (this.state != State.FINISHED) {
             if (this.state == State.CHOOSING_ACTION) {
-                System.out.println("Write action (buy, fill, take, remaining, exit):");
+                System.out.print("\nChoose an action (buy, remaining, fill, take, exit): ");
             }
             doStuff(scanner.next());
         }
     }
 
-    void doStuff(String input) {
+    void doStuff(final String input) {
         switch (this.state) {
             case CHOOSING_ACTION:
                 switch (input) {
                     case "buy":
                         this.state = State.CHOOSING_PRODUCT;
-                        System.out.print("\nWhat do you want to buy? 1 - espresso, 2 - latte,");
-                        System.out.println(" 3 - cappuccino, back - to main menu:");
-                        break;
-                    case "fill":
-                        this.state = State.FILLING_WATER;
-                        System.out.println("\nWrite how many ml of water do you want to add:");
+                        System.out.print("What do you want to buy? (espresso, latte, cappuccino, back): ");
                         break;
                     case "remaining":
                         printRemaining();
+                        break;
+                    case "fill":
+                        this.state = State.FILLING_WATER;
+                        System.out.print("How many ml of water do you want to add? ");
                         break;
                     case "take":
                         takeMoney();
@@ -76,10 +76,8 @@ public class CoffeeMachine {
                 }
                 break;
             case CHOOSING_PRODUCT:
-                if (!input.equals("back")) {
-                    if (checkIngredients(input)) {
-                        brew();
-                    }
+                if (!"back".equals(input) && checkIngredients(input)) {
+                    brew();
                 }
                 this.state = State.CHOOSING_ACTION;
                 break;
@@ -98,87 +96,92 @@ public class CoffeeMachine {
         }
     }
 
-    boolean checkIngredients(String type) {
+    boolean checkIngredients(final String type) {
         switch (type) {
-            case "1":
-                requiredWater = waterPerCupOfEspresso;
-                requiredMilk = milkPerCupOfEspresso;
-                requiredBeans = beansPerCupOfEspresso;
-                requiredMoney = costPerCupOfEspresso;
+            case "espresso":
+                requiredWater = WATER_PER_CUP_OF_ESPRESSO;
+                requiredMilk = MILK_PER_CUP_OF_ESPRESSO;
+                requiredBeans = BEANS_PER_CUP_OF_ESPRESSO;
+                requiredMoney = COST_PER_CUP_OF_ESPRESSO;
                 break;
-            case "2":
-                requiredWater = waterPerCupOfLatte;
-                requiredMilk = milkPerCupOfLatte;
-                requiredBeans = beansPerCupOfLatte;
-                requiredMoney = costPerCupOfLatte;
+            case "latte":
+                requiredWater = WATER_PER_CUP_OF_LATTE;
+                requiredMilk = MILK_PER_CUP_OF_LATTE;
+                requiredBeans = BEANS_PER_CUP_OF_LATTE;
+                requiredMoney = COST_PER_CUP_OF_LATTE;
                 break;
-            case "3":
-                requiredWater = waterPerCupOfCappuccino;
-                requiredMilk = milkPerCupOfCappuccino;
-                requiredBeans = beansPerCupOfCappuccino;
-                requiredMoney = costPerCupOfCappuccino;
+            case "cappuccino":
+                requiredWater = WATER_PER_CUP_OF_CAPPUCCINO;
+                requiredMilk = MILK_PER_CUP_OF_CAPPUCCINO;
+                requiredBeans = BEANS_PER_CUP_OF_CAPPUCCINO;
+                requiredMoney = COST_PER_CUP_OF_CAPPUCCINO;
                 break;
             default:
                 return false;
         }
-        if (storedWater < waterPerCupOfEspresso) {
-            System.out.println("Sorry, not enough water!\n");
-        } else if (storedMilk < requiredMilk) {
-            System.out.println("Sorry, not enough milk!\n");
-        } else if (storedBeans < requiredBeans) {
-            System.out.println("Sorry, not enough beans!\n");
-        } else if (storedCups < 1) {
-            System.out.println("Sorry, not enough cups!\n");
-        } else {
-            System.out.println("I have enough resources, making you a coffee!\n");
-            return true;
+        boolean hasEnoughResources = true;
+        if (storedWater < requiredWater) {
+            System.out.println("Sorry, not enough water!");
+            hasEnoughResources = false;
         }
-        return false;
+        if (storedMilk < requiredMilk) {
+            System.out.println("Sorry, not enough milk!");
+            hasEnoughResources = false;
+        }
+        if (storedBeans < requiredBeans) {
+            System.out.println("Sorry, not enough beans!");
+            hasEnoughResources = false;
+        }
+        if (storedCups < 1) {
+            System.out.println("Sorry, not enough cups!");
+            hasEnoughResources = false;
+        }
+        return hasEnoughResources;
     }
 
     void brew() {
+        System.out.println("Brewing you a coffee... done. Enjoy!");
         storedWater -= requiredWater;
         storedMilk -= requiredMilk;
         storedBeans -= requiredBeans;
-        storedMoney += requiredMoney;
         storedCups--;
+        storedMoney += requiredMoney;
     }
 
-    void fillWater(int amount) {
+    void fillWater(final int amount) {
         this.storedWater += amount;
         this.state = State.FILLING_MILK;
-        System.out.println("Write how many ml of milk do you want to add:");
+        System.out.print("How many ml of milk do you want to add? ");
     }
 
-    void fillMilk(int amount) {
+    void fillMilk(final int amount) {
         this.storedMilk += amount;
         this.state = State.FILLING_BEANS;
-        System.out.println("Write how many grams of coffee beans do you want to add:");
+        System.out.print("How many grams of coffee beans do you want to add? ");
     }
 
-    void fillBeans(int amount) {
+    void fillBeans(final int amount) {
         this.storedBeans += amount;
         this.state = State.FILLING_CUPS;
-        System.out.println("Write how many disposable cups of coffee do you want to add:");
+        System.out.print("How many disposable cups do you want to add? ");
     }
 
-    void fillCups(int amount) {
+    void fillCups(final int amount) {
         this.storedCups += amount;
         this.state = State.CHOOSING_ACTION;
-        System.out.println();
     }
 
     void takeMoney() {
-        System.out.println("\nI gave you $" + this.storedMoney);
+        System.out.println("\nIt gave you $" + this.storedMoney + ".");
         this.storedMoney = 0;
     }
 
     void printRemaining() {
-        System.out.println("\nThe coffee machine has:");
-        System.out.println(storedWater + " of water");
-        System.out.println(storedMilk + " of milk");
-        System.out.println(storedBeans + " of coffee beans");
-        System.out.println(storedCups + " of disposable cups");
-        System.out.println("$" + storedMoney + " of money\n");
+        System.out.println("The coffee machine has:");
+        System.out.println(storedWater + " ml of water");
+        System.out.println(storedMilk + " ml of milk");
+        System.out.println(storedBeans + " g of coffee beans");
+        System.out.println(storedCups + " disposable cups");
+        System.out.println("$" + storedMoney);
     }
 }
